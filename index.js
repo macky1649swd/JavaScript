fae7ddd97aa421393d3f383ac912b1f4ca9ecd9d
Hiroshi Matsuki <macky1649swd@gmail.com>
2023-01-06 18:36:24 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// This is my first javaScript
+console.log('Hello World');
+
+let myName = 'Hiroshi Matsuki';
+console.log(myName);
+
+const interestRate = 0.3;
+//interestRate = 1;
+console.log(interestRate);
+
+let selectedColors = ['red', 'blue'];
+
+console.log(selectedColors.length);
+
